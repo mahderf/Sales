3e12385ff73786a27a53e194bdc140263ffdaf5c
mahderf <30417140+mahderf@users.noreply.github.com>
2017-07-25 16:40:47 -0400

    update SalesReport.java

--- src/SalesReport.java
+++ src/SalesReport.java
@@ -10,38 +10,35 @@ public class SalesReport
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double SalesTax = 0;
-		double Totalamountdue=0;
+		double SalesTax = 0;  // intializing a variable
+		double Totalamountdue=0;  // initializing a variable
 	
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter your name: ");
-		String name = s.next();
-		System.out.println("Enter your Customer number: ");
-		int number = s.nextInt();
-		System.out.println(" Enter Sales amount: ");
-		double sales = s.nextDouble();
-		System.out.println("enter tax code: ");
-		int code = s.nextInt();
+		Scanner s = new Scanner(System.in); 
+		System.out.println("Enter your name: "); // prompts user to write name
+		String name = s.next();   // accept name
+		System.out.println("Enter your Customer number: "); // prompts user to wite customer number
+		int number = s.nextInt();  // accept number
+		System.out.println(" Enter Sales amount: ");  // prompts user to enter sales amount
+		double sales = s.nextDouble(); // accepts sales amount
+		System.out.println("enter tax code: "); // prompts user to enter tax code
+		int code = s.nextInt(); // accepts tax code
 		
-		//Totalamountdue= SalesTax + sales;
-		
-		//System.out.println(name + number + sales + code);
 	
-		if(code ==0)
+		if(code ==0)  // checks if tax code for a condition
 		{
-			SalesTax=0;
+			SalesTax=0;  // calculates sales tax
 		}
-		if (code ==1)
+		if (code ==1)  // checks if tax code for a condition
 		{
-	SalesTax= sales* 0.02;
+	SalesTax= sales* 0.02;  // calculates sales tax
 			
 		}
-		if (code==2)
+		if (code==2)  // checks if tax code for a condition
 		{
-			SalesTax= sales*0.05;
+			SalesTax= sales*0.05;  // calculates sales tax
 			
 		}
-		Totalamountdue= SalesTax + sales;
+		Totalamountdue= SalesTax + sales;  // calculates total amount due
 		
 System.out.println("Sales Report");
 System.out.println("Customer number: " + number);
